5702397547992e2555f7a5d8cb32a08472be4855
JiJogbf <alex.gas1995@ya.ru>
2021-03-20 21:54:47 +0300

    fixing install script

--- install.bat
+++ install.bat
@@ -1,2 +1,10 @@
 @echo off
 
+@REM указать переменную окружения 
+@REM установить туда библиотеку и заголовки
+
+setx GAS_CORE_HOME %cd%
+echo "gas library registered"
+set GAS_CORE_HOME
+
+@REM echo %userprofile%\documents
